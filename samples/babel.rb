f3963f0b5d10f56ba58d6c4fbcee2b4efa2d8a31
pascalbetz <pascal.betz@simplificator.com>
2009-07-13 17:54:59 +0200

    added sample

---
+++ samples/babel.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require File.dirname(__FILE__) + '/../lib/babel'  
+
+def guess_language(s)
+  puts "i think '#{s}' is #{s.language}"
+end
+
+def show_languages(s)
+  puts "Possible Languages for '#{s}': \n#{s.languages[0..2].join(', ')}"
+end
+ 
+en = "Rainy sunday"
+de = 'Regnerischer Sonntag'
+fr = 'Je ne regrette rien'
+sp = 'yo no soy un marinero'
+
+texts = [en, de, fr, sp]    
+Babel.load_profiles
+
+texts.each do |text|
+  guess_language text
+  show_languages text
+end  
+  
+  
+  
+
+  
--- test/babel_test.rb
+++ test/babel_test.rb
@@ -3,40 +3,8 @@ require 'test_helper'
 class BabelTest < Test::Unit::TestCase
   
   
-  should 'Build the profile' do
-    en = "Rainy sunday"
-    de = 'Regnerischer Sonntag'
-    fr = 'Je ne regrette rien'
-    sp = 'yo no soy un marinero'
-    
-    #Babel.load_profiles
-    Babel.load_profile File.dirname(__FILE__) + '/../lib/profiles/profile_deu.yml'
-    Babel.load_profile File.dirname(__FILE__) + '/../lib/profiles/profile_eng.yml'
-    Babel.load_profile File.dirname(__FILE__) + '/../lib/profiles/profile_ita.yml'
-    Babel.load_profile File.dirname(__FILE__) + '/../lib/profiles/profile_spa.yml'
-    Babel.load_profile File.dirname(__FILE__) + '/../lib/profiles/profile_fra.yml'
-    foo en
-    foo de
-    foo fr
-    foo sp
-    
-    bar en
-    bar de
-    bar fr
-    bar sp
-    
-    puts sp.languages[0..2].inspect
-  end
-  
-  
-  
-  
-  def foo(s)
-    puts "i think '#{s}' is #{s.language}"
-  end
-  
-  def bar(s)
-    puts "Distances for '#{s}': \n#{Babel.distances(s).inspect}"
+  should 'have a test' do
+   fail
   end
   
 end

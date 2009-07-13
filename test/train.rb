e4bc160b49f6ded918822eec1b648e2bb94a1e74
pascalbetz <pascal.betz@simplificator.com>
2009-07-13 11:32:46 +0200

    removed old profiles

--- test/train.rb
+++ test/train.rb
@@ -6,8 +6,8 @@ class TrainTest < Test::Unit::TestCase
   should 'Build the profiles' do
    Dir[File.dirname(__FILE__) + '/../lib/data/*.txt'].each do |name|
      File.basename(name) =~ /udhr\_(.+)\.txt/
-     #if ['eng', 'fra', 'deu', 'spa', 'ita'].include?($1)
-     if ['deu'].include?($1)
+     if ['eng', 'fra', 'deu', 'spa', 'ita'].include?($1)
+     #if ['deu'].include?($1)
      puts "Learning '#{$1}'"
      File.open(name, 'r') do |file|
        file.each_with_index do |line, index|
